79c81445a66c8eeab4920ae3de2c828e93d9511f
AbdullahCelik0 <140632591+AbdullahCelik0@users.noreply.github.com>
2023-07-31 19:34:54 +0200

    3.version

---
+++ src/Day02/Github_day02.java
@@ -0,0 +1,8 @@
+package Day02;
+
+public class Github_day02 {
+    public static void main(String[] args) {
+        System.out.println("Tekrar merhaba Github");
+        System.out.println("bende buradayım");
+    }
+}
