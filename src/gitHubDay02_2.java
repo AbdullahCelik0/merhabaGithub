f709f6f88f97329d1dd2242756ea3130b14e783b
AbdullahCelik0 <140632591+AbdullahCelik0@users.noreply.github.com>
2023-07-31 20:14:47 +0200

    gule gule v3

--- src/gitHubDay02_2.java
+++ src/gitHubDay02_2.java
@@ -1,5 +1,6 @@
 public class gitHubDay02_2 {
     public static void main(String[] args) {
         System.out.println("2. dosya 1. satır. Denemeye devam");
+        System.out.println("denemeye dep devam");
     }
 }
